af517bd286fd870dae69f1f7e1ce168ea7360ad4
iketanwagh <iketanwagh@gmail.com>
2025-05-18 12:27:12 +0530

    factorial practice

---
+++ factorial.py
@@ -0,0 +1,14 @@
+# Assignment #7: Factorial Calculator
+# Task:
+# Write a program to calculate the factorial of a number.
+
+# Example:
+
+# Input: 5
+# Output: 120 (because 5×4×3×2×1 = 120)
+
+# Requirements:
+
+# Take a non-negative integer input.
+# Use a for loop to calculate factorial.
+# Print the result.
