976d181c2500611208fc5242358c48fe3db69d72
iketanwagh <iketanwagh@gmail.com>
2025-05-18 12:30:05 +0530

    file of factorial added to master

--- factorial.py
+++ factorial.py
@@ -12,3 +12,14 @@
 # Take a non-negative integer input.
 # Use a for loop to calculate factorial.
 # Print the result.
+
+input_number = int(input("Enter a non-negative integer: "))
+
+# Check if the number is negative
+if input_number < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    factorial = 1
+    for i in range(1, input_number + 1):
+        factorial *= i  # Multiply factorial by each number from 1 to input_number
+    print(f"Factorial of {input_number} is: {factorial}")
